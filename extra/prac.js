2574c6e4dd45c4c58c5c23df354414d7984a807c
HeEt <shahheet2809@gmail.com>
2026-06-11 16:29:41 +0530

    Initial commit: Add index.html

---
+++ extra/prac.js
@@ -0,0 +1,17 @@
+const cars = ["Saab","BMW","Merc"]
+
+console.log(cars[0]);
+console.log(cars[2]);
+console.log(cars[1]);
+
+var globVar = " Supp its Global variable";
+
+function exFunc()
+    {
+    console.log(globVar);
+    console.log("Its me another text")
+    }
+
+exFunc();
+console.log(globVar )
+
